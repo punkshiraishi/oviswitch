94b3fcd0a93bccc5f0f0afd79c248e47824914f1
shiraishi <shiraishi_yuma@uniquevision.co.jp>
2022-08-10 17:29:45 +0900

    delete: views of content scripts

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,7 +1,5 @@
 /* eslint-disable no-console */
 import { onMessage, sendMessage } from 'webext-bridge'
-import { createApp } from 'vue'
-import App from './views/App.vue'
 
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (() => {
@@ -24,16 +22,4 @@ import App from './views/App.vue'
 
   // アイコンボタンがクリックされたとき → ここで listener を埋め込む
   // にミュート状態取得 アイコン変更処理 → アイコン変更は background にメッセージ送信をして行う
-
-  // mount component to context window
-  const container = document.createElement('div')
-  const root = document.createElement('div')
-  const styleEl = document.createElement('link')
-  const shadowDOM = container.attachShadow?.({ mode: __DEV__ ? 'open' : 'closed' }) || container
-  styleEl.setAttribute('rel', 'stylesheet')
-  styleEl.setAttribute('href', browser.runtime.getURL('dist/contentScripts/style.css'))
-  shadowDOM.appendChild(styleEl)
-  shadowDOM.appendChild(root)
-  document.body.appendChild(container)
-  createApp(App).mount(root)
 })()
